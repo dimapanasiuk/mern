bb256bb4aaacd4fd27c4345961d4703ff3c3c1ec
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-03 12:46:34 +0300

    refactor: add normalize

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./normalize.css";
 
 ReactDOM.render(
   <Router>
