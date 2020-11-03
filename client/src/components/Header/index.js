2a61864755c515747a1c0a0c84382a4609e87185
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-03 13:09:27 +0300

    feat: implementation header with reactstrap and styled components

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+import { Collapse, Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
+
+const RouterLink = styled(Link)`
+  color: white;
+  margin-right: 10px;
+  text-decoration: none;
+
+  :hover {
+    color: red;
+  }
+`;
 
 const Header = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/cabinet">Cabinet</Link>
-        </li>
-        <li>
-          <Link to="/registration">Registration</Link>
-        </li>
-      </ul>
-    </nav>
+    <div>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand>
+          <RouterLink to="/">Home</RouterLink>
+        </NavbarBrand>
+        <Collapse navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <RouterLink to="/login">Login</RouterLink>
+            </NavItem>
+            <NavItem>
+              <RouterLink to="/registration">Registration</RouterLink>
+            </NavItem>
+            <NavItem>
+              <RouterLink to="/cabinet">Cabinet</RouterLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 };
 
--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,19 +1,5 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
-import { Button } from "reactstrap";
 import axios from "axios";
-
-const TestButton = styled.button`
-  font-size: 1em;
-  background-color: black;
-  color: white;
-  border: none;
-`;
-
-const CustomBox = styled("div")`
-  width: 300px;
-  background-color: ${(props) => (props ? "red" : "green")};
-`;
 
 const Home = () => {
   const [user, setUser] = useState("");
@@ -30,7 +16,6 @@ const Home = () => {
   }, []);
 
   const userData = () => {
-    console.log("----------------user", user);
     if (!user) {
       return (
         <p>
@@ -45,18 +30,10 @@ const Home = () => {
       );
     }
   };
-  
+
   return (
     <>
       <h1> Home</h1>
-      <TestButton>Styledcomponent button </TestButton>
-      <Button color="danger">Reactstrap button</Button>
-      <CustomBox active={1}>
-        <h2>
-          Put any other react or custom styled-component within this component
-        </h2>
-        <Button>Click me</Button>
-      </CustomBox>
       {userData()}
     </>
   );
