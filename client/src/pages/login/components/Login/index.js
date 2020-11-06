c88e22de107d2616b76fa0e06ea40199bd226211
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-06 14:08:31 +0300

    refactor: make login use jsx and axios

--- client/src/pages/login/components/Login/index.js
+++ client/src/pages/login/components/Login/index.js
@@ -1,21 +1,58 @@
 import React from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import { Button, FormGroup, Label, Input as StrapInput } from "reactstrap";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
+  const history = useHistory();
+  const { register, handleSubmit, errors } = useForm();
+
+  const submitHandler = (requestData) => {
+    axios
+      .post("/login", requestData)
+      .then((response) => {
+        if (response.data) {
+          history.push("/");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const STR = "please enter";
+
   return (
     <>
-      <form action="/login" method="post">
-        <div>
-          <p>Username:</p>
-          <input type="text" name="username" />
-          <br />
-        </div>
-        <div>
-          <p>Password:</p>
-          <input type="password" name="password" />
-        </div>
-        <div>
-          <input type="submit" value="Submit" />
-        </div>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <FormGroup>
+          <Label for="login">Login</Label>
+          <StrapInput
+            id="login"
+            placeholder={`${STR} login`}
+            name="username"
+            innerRef={register}
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label>Password</Label>
+          <StrapInput
+            placeholder={`${STR} password`}
+            name="password"
+            type="password"
+            innerRef={register({
+              required: true,
+              minLength: {
+                value: 5,
+                message: "error message min", // <p>error message</p>
+              },
+            })}
+          />
+
+          {errors.password && <span>This field is required</span>}
+        </FormGroup>
+        <Button type="submit"> Submit</Button>
       </form>
     </>
   );
--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,20 +1,8 @@
-import React, { useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import Login from "./components/Login";
 
 const LoginPage = () => {
-  useEffect(() => {
-    axios
-      .get("https://statsapi.web.nhl.com/api/v1/teams")
-      .then((response) => {
-        console.log("--------------", response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
   return <Login />;
 };
 
