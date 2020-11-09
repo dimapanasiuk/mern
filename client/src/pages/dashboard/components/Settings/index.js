6ab23ad5cc16492a65c45f76bce8fc7861af67ef
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-09 12:45:26 +0300

    feat: add save button and axios for get info to server

--- client/src/pages/dashboard/components/Settings/index.js
+++ client/src/pages/dashboard/components/Settings/index.js
@@ -1,17 +1,40 @@
 import React from "react";
-import { any } from "prop-types";
+import { any, array } from "prop-types";
+import { Button } from "reactstrap";
+import { connect } from "react-redux";
+import axios from "axios";
 
-const DashSettings = ({ child }) => {
+const DashSettings = ({ child, saveTeams }) => {
+  const saveClickHandler = async () => {
+    axios
+      .put("/save", { teams: saveTeams })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <>
       <h1>DashSettings</h1>
       {child[0]()}
+      <Button color="success" onClick={saveClickHandler}>
+        Save Changes
+      </Button>{" "}
     </>
   );
 };
 
 DashSettings.propTypes = {
-  child: any, 
+  child: any,
+  saveTeams: array,
 };
 
-export default DashSettings;
+const mapDispatchToProps = (state) => {
+  console.log("Teams component", state.choseTeamsReducer.teams);
+  return { saveTeams: state.choseTeamsReducer.teams };
+};
+
+export default connect(mapDispatchToProps)(DashSettings);
