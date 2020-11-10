c5b0c283914f4274f53bc3f052722c3a9f37047a
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-10 17:16:37 +0300

    feat: add char js

--- client/src/pages/dashboard/components/Bank/Chart/index.js
+++ client/src/pages/dashboard/components/Bank/Chart/index.js
@@ -1,7 +1,63 @@
-import React from "react";
+import React, { useEffect } from "react";
+import ChartJS from "chart.js";
+import { array } from "prop-types";
 
-const Chart = () => {
-  return <h1>Chart</h1>;
+const Chart = ({ data }) => {
+  useEffect(() => {
+    const ctx = document.getElementById("myChart");
+    // eslint-disable-next-line no-new
+    new ChartJS(ctx, {
+      type: "bar",
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+              },
+            },
+          ],
+        },
+      },
+    });
+  });
+
+  console.log("data", data);
+  return (
+    <div className="App">
+      <canvas id="myChart" width="400" height="400" />
+    </div>
+  );
+};
+
+Chart.propTypes = {
+  data: array,
 };
 
 export default Chart;
--- client/src/pages/dashboard/components/Bank/index.js
+++ client/src/pages/dashboard/components/Bank/index.js
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { array } from "prop-types";
 import { connect } from "react-redux";
 import Chart from "./Chart";
 
-const START = "2020-1-5";
-const END = "2020-1-20";
+const START = "2020-10-1";
+const END = "2020-10-11";
 
 const Bank = ({ ids }) => {
+  const [dataForChart, setDataForChart] = useState([]);
+
   useEffect(() => {
     if (ids.length) {
       const cur = ids[0];
@@ -16,11 +18,14 @@ const Bank = ({ ids }) => {
         .get(
           `https://www.nbrb.by/api/exrates/rates/dynamics/${cur}?startDate=${START}&endDate=${END}`
         )
-        .then((data) => console.log("bank", data.data));
+        .then((data) => {
+          setDataForChart(data.data);
+          console.log('data', data);
+        });
     }
   }, [ids]);
 
-  return <Chart />;
+  return <Chart data={dataForChart} />;
 };
 
 Bank.propTypes = {
@@ -30,7 +35,6 @@ Bank.propTypes = {
 const mapDispatchToProps = (state) => {
   const { currencies } = state.choseCurrenciesIdReducer;
   const curs = currencies.map((currency) => currency.id);
-  console.log("curs", curs);
   return {
     ids: curs,
   };
