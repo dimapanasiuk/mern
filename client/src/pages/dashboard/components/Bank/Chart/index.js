813dfb219cd8ccabeeafeb3636eda647a6be1432
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-11 15:05:25 +0300

    feat: add new line to charjs

--- client/src/pages/dashboard/components/Bank/Chart/index.js
+++ client/src/pages/dashboard/components/Bank/Chart/index.js
@@ -9,22 +9,15 @@ const chartData = {
       label: "My First dataset",
       fill: false,
       lineTension: 0.1,
-      backgroundColor: "rgba(75,192,192,0.4)",
       borderColor: "rgba(75,192,192,1)",
-      borderCapStyle: "butt",
-      borderDash: [],
-      borderDashOffset: 0.0,
-      borderJoinStyle: "miter",
-      pointBorderColor: "rgba(75,192,192,1)",
-      pointBackgroundColor: "#fff",
-      pointBorderWidth: 1,
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(75,192,192,1)",
-      pointHoverBorderColor: "rgba(220,220,220,1)",
-      pointHoverBorderWidth: 2,
-      pointRadius: 1,
-      pointHitRadius: 10,
       data: [65, 59, 80, 81, 56, 55, 40],
+    },
+    {
+      label: "National Average",
+      fill: false,
+      lineTension: 0.1,
+      borderColor: "red",
+      data: [65, 20, 30, 10, 56, 56, 46],
     },
   ],
 };
@@ -33,7 +26,7 @@ const Chart = ({ data }) => {
   console.log(data);
   return (
     <>
-      <Line data={chartData} />
+      <Line data={chartData}  height="80"/>
     </>
   );
 };
