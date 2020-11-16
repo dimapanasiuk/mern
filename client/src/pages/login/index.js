4f73fe29c8b20c64be584246eb134b75103f0da4
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-16 16:50:52 +0300

    refactor: login refactor

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -1,9 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
 
 import Login from "./components/Login";
 
 const LoginPage = () => {
-  return <Login />;
+  return (
+    <>
+      <Login />
+      <Breadcrumb>
+        <BreadcrumbItem active>
+          <NavLink exact to="/profile">
+            Profile
+          </NavLink>
+        </BreadcrumbItem>
+        <BreadcrumbItem active>
+          <NavLink exact to="/registration">
+            Registration
+          </NavLink>
+        </BreadcrumbItem>
+      </Breadcrumb>
+    </>
+  );
 };
 
 export default LoginPage;
