04f5780cd8b3dfc228d652b9b7910236cf7c9bfc
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-17 17:09:07 +0300

    refactor: some refactoring

--- client/src/pages/dashboard/components/Map/Places/index.js
+++ client/src/pages/dashboard/components/Map/Places/index.js
@@ -1,59 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
-import { CopyToClipboard } from "react-copy-to-clipboard";
-import { Button } from "reactstrap";
 
 const Places = () => {
-  const [rows, setRows] = useState([]);
-
   return (
-    <div className="App">
+    <>
       <GooglePlacesAutocomplete
         placeholder="Type in an address"
         inputStyle={{
           height: 40,
           fontSize: 28,
         }}
-        suggestionsStyles={{
-          container: {
-            padding: 16,
-            background: "#efefef",
-          },
-          suggestion: {
-            background: "#eee",
-            cursor: "pointer",
-          },
-          suggestionActive: {
-            background: "#bbb",
-          },
-        }}
-        onSelect={(result) => {
-          // eslint-disable-next-line camelcase
-          const { description, place_id } = result;
-
-          setRows([{ description, place_id }, ...rows]);
-        }}
       />
-      <br />
-
-      <div
-        style={{
-          textAlign: "left",
-        }}
-      >
-        {rows.map((row) => (
-          <div key={row.place_id} style={{ padding: "8px 0" }}>
-            <div style={{ fontSize: 12, color: "#bbb" }}>{row.description}</div>
-            <div>
-              {row.place_id}&nbsp;
-              <CopyToClipboard text={row.place_id}>
-                <Button>Copy</Button>
-              </CopyToClipboard>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
+    </>
   );
 };
 
