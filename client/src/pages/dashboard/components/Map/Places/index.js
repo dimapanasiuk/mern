7d1fc827bcd31e284bd65c3384cf60f1a06a35c4
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-18 11:48:51 +0300

    refactor: some refactoring

--- client/src/pages/dashboard/components/Map/Places/index.js
+++ client/src/pages/dashboard/components/Map/Places/index.js
@@ -1,23 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import GooglePlacesAutocomplete from "react-google-places-autocomplete";
 import { Div } from "./style";
 
 // work with this key
 // https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJk1uS2eG7FkgRqzCcF1iDSMY&fields=name,rating,geometry,formatted_phone_number&key=AIzaSyCuMJ3dhADqNoE4tGuWTI3_NlwBihj5BtE
 
-const request =
-  "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJk1uS2eG7FkgRqzCcF1iDSMY&fields=name,rating,geometry,formatted_phone_number&key=AIzaSyCuMJ3dhADqNoE4tGuWTI3_NlwBihj5BtE";
+//  "https://maps.googleapis.com/maps/api/place/details/json?place_id=ChIJk1uS2eG7FkgRqzCcF1iDSMY&fields=name,rating,geometry,formatted_phone_number&key=AIzaSyCuMJ3dhADqNoE4tGuWTI3_NlwBihj5BtE";
 
 const Places = () => {
   const [value, setValue] = useState(null);
-
-  useEffect(() => {
-    axios
-      .get(request)
-      .then((data) => console.log(data.data))
-      .catch((e) => console.warn("💡🛑", e));
-  }, [value]);
 
   return (
     <>
