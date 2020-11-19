9aa5a854677f6acc0635e727e1b11b434d3555df
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-19 10:39:21 +0300

    feat: add useForm for sendFeedback

--- client/src/pages/dashboard/components/Map/SendFeedback/index.js
+++ client/src/pages/dashboard/components/Map/SendFeedback/index.js
@@ -1,19 +1,26 @@
 import React from "react";
 import { Button, FormGroup, Label, Input } from "reactstrap";
+import { useForm } from "react-hook-form";
 
 import Places from "../Places";
 import { FormStyle } from "./style";
 
 const SendFeedback = () => {
+  const { register, handleSubmit } = useForm();
+
+  const submitHandler = (requestData) => {
+    console.log("requestData", requestData);
+  };
+
   return (
-    <FormStyle>
+    <FormStyle onSubmit={handleSubmit(submitHandler)}>
       <FormGroup>
         <Label>Please enter place</Label>
-        <Places />
+        <Places innerRef={register} />
       </FormGroup>
       <FormGroup style={{ height: "100%" }}>
-        <Label for="exampleText">Write description</Label>
-        <Input type="textarea" name="text" id="exampleText" />
+        <Label>Write description</Label>
+        <Input type="textarea" name="description" innerRef={register} />
       </FormGroup>
       <Button>Submit</Button>
     </FormStyle>
