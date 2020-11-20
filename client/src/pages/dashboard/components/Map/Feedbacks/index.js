25a38148c1b2420fc71cbbe7c01d456307f4192d
dimapanasiuk <dimapanasiuk@icloud.com>
2020-11-20 11:03:23 +0300

    feat: add style for feedbacks card

--- client/src/pages/dashboard/components/Map/Feedbacks/index.js
+++ client/src/pages/dashboard/components/Map/Feedbacks/index.js
@@ -1,19 +1,19 @@
 import React from "react";
 import { array } from "prop-types";
-import { Col, CardTitle, CardText } from "reactstrap";
+import { CardTitle, CardText } from "reactstrap";
 import { size } from "lodash";
 
-import { CardStyle } from "./style";
+import { CardStyle, ColStyle } from "./style";
 
 const Feedbacks = ({ feedbacks }) => {
   const cards = feedbacks.map((i) => {
     return (
-      <Col key={i.id} sm={6}>
+      <ColStyle key={i.id} sm={6}>
         <CardStyle>
           <CardTitle>{i.label} </CardTitle>
           <CardText>{i.desc}</CardText>
         </CardStyle>
-      </Col>
+      </ColStyle>
     );
   });
 
--- client/src/pages/dashboard/components/Map/Feedbacks/style.js
+++ client/src/pages/dashboard/components/Map/Feedbacks/style.js
@@ -1,7 +1,11 @@
-import { Card } from "reactstrap";
+import { Card, Col } from "reactstrap";
 import styled from "styled-components";
 
 export const CardStyle = styled(Card)`
   padding: 10px;
   margin-top: 20px;
+`;
+
+export const ColStyle = styled(Col)`
+  padding-left: 0px;
 `;
