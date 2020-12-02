0f4a472e705c05bbcd7c1c330effbde778678aa0
dimapanasiuk <dimapanasiuk@icloud.com>
2020-12-02 17:41:39 +0300

    refactor: division into folders and files

--- server/index.js
+++ server/index.js
@@ -6,17 +6,10 @@ const PORT = config.get("port") || 4000;
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const passport = require("passport");
-const Strategy = require("passport-local").Strategy;
-const connectEnsureLogin = require("connect-ensure-login");
 const session = require("express-session");
-const axios = require("axios").default;
 
-const db = require("./db");
-const User = require("./scheme/user");
 const routing = require('./routing');
 
-app.use('/', routing)
 
 async function start() {
   const url = config.get("mongoUri");
@@ -36,37 +29,6 @@ async function start() {
 start();
 
 app.use(cors());
-
-passport.use(
-  new Strategy(function (username, password, cb) {
-    db.users.findByUsername(username, function (err, user) {
-      if (err) {
-        return cb(err);
-      }
-      if (!user) {
-        return cb(null, false);
-      }
-      if (user.password != password) {
-        return cb(null, false);
-      }
-      return cb(null, user);
-    });
-  })
-);
-
-passport.serializeUser(function (user, cb) {
-  cb(null, user._id);
-});
-
-passport.deserializeUser(function (id, cb) {
-  db.users.findById(id, function (err, user) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, user);
-  });
-});
-
 app.use(morgan("combined"));
 
 app.use(bodyParser.json());
@@ -80,70 +42,6 @@ app.use(
   })
 );
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get("/home", function (req, res) {
-  res.send({ user: req.user });
-});
-
-app.post(
-  "/login",
-  passport.authenticate("local", { failureRedirect: "/login" }),
-  (req, res) => {
-    res.redirect("/home");
-  }
-);
-
-app.get("/logout", (req, res) => {
-  req.logout();
-  res.redirect("/home");
-});
-
-app.get("/profile", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
-  res.send({ user: req.user });
-});
-
-app.post("/registration", (req, res) => {
-  const { password, password2 } = req.body;
-
-  if (password === password2 && password.length > 4) {
-    const user = new User({
-      name: req.body.userName,
-      password: req.body.password,
-    });
-
-    user.save((e) => {
-      if (e) return console.error("=====💡🛑=====", e);
-    });
-
-    res.send({ data: "work" });
-  }
-
-  res.send({ data: "err" });
-});
-
-app.put("/save", (req, res) => {
-  const { teams } = req.body;
-
-  User.updateOne({ name: "dima" }, { nhlTeams: teams }, function (e, result) {
-    if (e) return console.error("=====💡🛑=====", e);
-  });
-
-  res.send(req.body.teams);
-});
-
-app.post("/map", (req, res) => {
-  const { placeId, API_KEY } = req.body;
-
-  const request = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,geometry,formatted_phone_number&key=${API_KEY}`;
-
-  axios
-    .get(request)
-    .then((data) => {
-      res.send(data.data);
-    })
-    .catch((e) => console.error("=====💡🛑=====", e));
-});
+app.use('/', routing);
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
--- server/routing/index.js
+++ server/routing/index.js
@@ -1,7 +1,116 @@
 const express = require('express');
 const router = express.Router();
+const axios = require("axios").default;
+const connectEnsureLogin = require("connect-ensure-login");
 
+const passport = require("passport");
+const Strategy = require("passport-local").Strategy;
+
+const User = require("../scheme/user");
+const db = require("../db");
+
+express().use(passport.initialize());
+express().use(passport.session());
+
+passport.use(
+	new Strategy(function (username, password, cb) {
+		db.users.findByUsername(username, function (err, user) {
+			if (err) {
+				return cb(err);
+			}
+			if (!user) {
+				return cb(null, false);
+			}
+			if (user.password != password) {
+				return cb(null, false);
+			}
+			return cb(null, user);
+		});
+	})
+);
+
+passport.serializeUser(function (user, cb) {
+	cb(null, user._id);
+});
+
+passport.deserializeUser(function (id, cb) {
+	db.users.findById(id, function (err, user) {
+		if (err) {
+			return cb(err);
+		}
+		cb(null, user);
+	});
+});
 
 router.get("/", (req, res) => {
-    res.send("<h1>Test</h1>");
-  });
+	res.send("<h1>Test</h1>");
+});
+
+router.get("/home", function (req, res) {
+	res.send({ user: req.user });
+});
+
+router.post(
+	"/login",
+	passport.authenticate("local", { failureRedirect: "/login" }),
+	(req, res) => {
+		res.redirect("/home");
+	}
+);
+
+router.get("/logout", (req, res) => {
+	req.logout();
+	res.redirect("/home");
+});
+
+router.get("/profile", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
+	res.send({ user: req.user });
+});
+
+router.post("/registration", (req, res) => {
+	const { userName, password, password2 } = req.body;
+
+	console.log('userName', userName);
+	console.log('password', password);
+
+
+	if (password === password2 && password.length > 4) {
+		const user = new User({
+			name: userName,
+			password: password,
+		});
+
+		user.save((e) => {
+			if (e) return console.error("=====💡🛑=====", e);
+		});
+
+		res.send({ data: "work" });
+	}
+
+	res.send({ data: "err" });
+});
+
+router.put("/save", (req, res) => {
+	const { teams } = req.body;
+
+	User.updateOne({ name: "dima" }, { nhlTeams: teams }, function (e, result) {
+		if (e) return console.error("=====💡🛑=====", e);
+	});
+
+	res.send(req.body.teams);
+});
+
+router.post("/map", (req, res) => {
+	const { placeId, API_KEY } = req.body;
+
+	const request = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,geometry,formatted_phone_number&key=${API_KEY}`;
+
+	axios
+		.get(request)
+		.then((data) => {
+			res.send(data.data);
+		})
+		.catch((e) => console.error("=====💡🛑=====", e));
+});
+
+module.exports = router;
