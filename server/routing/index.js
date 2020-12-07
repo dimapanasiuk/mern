cb6f4c40850a02509a32b3f3a27816b65767157b
dimapanasiuk <dimapanasiuk@icloud.com>
2020-12-07 15:10:47 +0300

    refactor: some refactor

--- server/routing/index.js
+++ server/routing/index.js
@@ -10,105 +10,99 @@ const User = require("../scheme/user");
 const db = require("../db");
 
 passport.use(
-	new Strategy(function (username, password, cb) {
-		 db.users.findByUsername(username, function (err, user) {
-			if (err) {
-				return cb(err);
-			}
-			if (!user) {
-				return cb(null, false);
-			}
-			if (user.password != password) {
-				return cb(null, false);
-			}
-			return cb(null, user);
-		});
-	})
+  new Strategy(function (username, password, cb) {
+    db.users.findByUsername(username, function (err, user) {
+      if (err) {
+        return cb(err);
+      }
+      if (!user) {
+        return cb(null, false);
+      }
+      if (user.password != password) {
+        return cb(null, false);
+      }
+      return cb(null, user);
+    });
+  })
 );
 
-passport.serializeUser(function (user, cb) {
-	cb(null, user._id);
+passport.serializeUser((user, cb) => {
+  cb(null, user._id);
 });
 
-passport.deserializeUser(function (id, cb) {
-	db.users.findById(id, function (err, user) { // ask this
-		if (err) {
-			return cb(err);
-		}
-		cb(null, user);
-	});
+passport.deserializeUser((id, cb) => {
+  db.users.findById(id, (err, user) => { // ask this
+    if (err) {
+      return cb(err);
+    }
+    cb(null, user);
+  });
 });
 
-router.get("/", (req, res) => {
-	res.send("<h1>Test</h1>");
-});
+router.get("/home", (req, res) => { // try/catch at each endpoint
+  
+  res.send({ user: req.user });
 
-router.get("/home", function (req, res) { //to arrow function, try/catch at each endpoint
-	res.send({ user: req.user });
+  throw new Error('Something broke! ');
 });
 
 router.post(
-	"/login",
-	passport.authenticate("local", { failureRedirect: "/login" }),
-	(req, res) => {
-		res.redirect("/home");
-	}
+  "/login",
+  passport.authenticate("local", { failureRedirect: "/login" }),
+  (req, res) => {
+    res.redirect("/home");
+  }
 );
 
-router.get("/logout", (req, res) => { // logout must be post
-	req.logout();
-	res.redirect("/home");
+router.post("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/home");
 });
 
-router.get("/profile", connectEnsureLogin.ensureLoggedIn(), function (req, res) {
-	res.send({ user: req.user });
+router.get("/profile", connectEnsureLogin.ensureLoggedIn(), (req, res) => {
+  res.send({ user: req.user });
 });
 
 router.post("/registration", (req, res) => {
-	const { userName, password, password2 } = req.body;
+  const { userName, password, password2 } = req.body;
 
-	console.log('userName', userName);
-	console.log('password', password);
+  if (password === password2 && password.length > 4) {
+    const user = new User({
+      name: userName,
+      password: password,
+    });
 
+    user.save((e) => {
+      if (e) return console.error("=====💡🛑=====", e);
+    });
 
-	if (password === password2 && password.length > 4) {
-		const user = new User({
-			name: userName,
-			password: password,
-		});
+    res.send({ data: "work" });
+  }
 
-		user.save((e) => {
-			if (e) return console.error("=====💡🛑=====", e);
-		});
-
-		res.send({ data: "work" });
-	}
-
-	res.send({ data: "err" });
+  res.send({ data: "err" });
 });
 
 router.put("/save", (req, res) => {
-	const { teams } = req.body;
+  const { teams } = req.body;
 
-	User.updateOne({ name: "dima" }, { nhlTeams: teams }, function (e, result) {
-		if (e) return console.error("=====💡🛑=====", e);
-	});
+  User.updateOne({ name: "dima" }, { nhlTeams: teams }, (e, result) => {
+    if (e) return console.error("=====💡🛑=====", e);
+  });
 
-	res.send(req.body.teams);
+  res.send(req.body.teams);
 });
 
 router.post("/map", (req, res) => { //?
-	const { placeId, API_KEY } = req.body;
+  const { placeId, API_KEY } = req.body;
 
-	const request = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,geometry,formatted_phone_number&key=${API_KEY}`;
+  const request = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=name,rating,geometry,formatted_phone_number&key=${API_KEY}`;
 
-	axios
-		.get(request)
-		.then((data) => {
-			res.send(data.data);
-		})
-		.catch((e) => console.error("=====💡🛑=====", e));
+  axios
+    .get(request)
+    .then((data) => {
+      res.send(data.data);
+    })
+    .catch((e) => console.error("=====💡🛑=====", e));
 });
 
 module.exports = router;
-// always
